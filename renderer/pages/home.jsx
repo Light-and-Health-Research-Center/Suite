287c4d343ae8f5110fd9c189a11e1bade73b79dd
vance <root.mvm@gmail.com>
2021-10-02 20:27:13 -0400

    A change

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -23,6 +23,7 @@ function Home() {
       <Head>
         <title>Home</title>
       </Head>
+      <p>A CHNAGE</p>
       <p>CPU Usage: {cpuUsage.toFixed(2)}</p>
       <p>System Memory: {systemMemory.toFixed(2)} GB</p>
       <p>Free Memory: {freeMemoryPercentage.toFixed(2)} %</p>
