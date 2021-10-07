1a299daf35f12c13c9604bfa96a738a4ebe30288
vance <root.mvm@gmail.com>
2021-10-07 01:39:42 -0400

    - Correctly display application title - reduce height of tapbar

--- renderer/components/TopBar.js
+++ renderer/components/TopBar.js
@@ -1,9 +1,23 @@
 import WindowButtons from "./WindowButtons";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+const titles = {
+  "/home": "LHRC Suite",
+  "/rvp": "rvp calculator",
+  "/zonal": "Zonal Cavity Calculator",
+};
 
 export default function TopBar() {
+  const router = useRouter();
+  const [title, setTitle] = useState(titles[router.pathname]);
+
+  useEffect(() => {
+    setTitle(titles[router.pathname]);
+  }, [router]);
   return (
-    <div className="bg-blueGray-700 h-14 window-drag flex justify-between text-white-100">
-      <h1 className="pl-6 my-auto font-semibold text-md">RVP CALCULATOR</h1>
+    <div className="bg-blueGray-700 h-11 window-drag flex justify-between text-white-100">
+      <h1 className="pl-6 my-auto font-semibold text-md uppercase">{title}</h1>
       <WindowButtons />
     </div>
   );
